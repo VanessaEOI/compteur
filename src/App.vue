b6665286e55106aa04ffd9d9516bdd5b52c929de
Vanessa Estival <vel@ehtrace.com>
2023-05-15 16:35:54 +0200

    Add componentÃ

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import buttons from "./components/buttons.vue";
+
+</script>
+
+<template>
+
+    <h1>Compteur</h1>
+
+    <div>
+        <buttons/>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/buttons.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const count = ref(0)
+
+  function minusFive() {
+      count.value = - 5
+  }
+
+  function minusOne() {
+      count.value = - 1
+  }
+
+  function reset() {
+      count.value = 0
+  }
+
+  function plusOne() {
+      count.value = + 1
+  }
+
+  function plusFive() {
+      count.value = + 5
+  }
+
+</script>
+
+<template>
+    <label>Le compteur affiche : {{ count }}</label>
+    <div>
+        <button type="button" @click="minusFive()">- 5</button>
+        <button type="button" @click="minusOne">- 1</button>
+        <button type="button" @click="reset">0</button>
+        <button type="button" @click="plusOne">1</button>
+        <button type="button" @click="plusFive">5</button>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
