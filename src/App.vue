73452518c134c88bb8e73e712dea2dbc18c8c817
Vanessa Estival <vel@ehtrace.com>
2023-08-30 14:54:02 +0200

    Working useRefHistory

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
 
 import ActionButton from "./components/ActionButton.vue";
-
 import {ref} from 'vue'
-import {useStorage} from "@vueuse/core";
-
+import {useRefHistory, useStorage, useTemplateRefsList} from "@vueuse/core";
+import {nextTick, watch} from "@vue/runtime-core";
 
 const count = ref(0)
+const refs = useTemplateRefsList<HTMLDivElement>()
+const { history, undo, redo, canUndo, canRedo } = useRefHistory(count, { capacity: 10 })
+
+watch(refs, async () => {
+  await nextTick()
+  console.log([...refs["value"]])
+}, {
+  deep: true,
+  flush: 'post',
+})
 
 useStorage('count', count)
-
 
 const dataButtons = ref([
   {id: 1, label: '-1', value: -1},
@@ -31,6 +39,14 @@ const dataButtons = ref([
                   :value-button="dataButton.value"
                   :key="dataButton.id"
                   v-model="count"/>
+  </div>
+  <div>
+    <button :disabled="!canUndo" @click="undo()">Undo</button>
+    <button :disabled="!canRedo" @click="redo()">Redo</button>
+  </div>
+
+  <div v-for="i in history" :key="i.timestamp">
+    <span>Valeur du compteur : {{ i.snapshot }} </span>
   </div>
 
 </template>
