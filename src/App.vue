9ccd5714d91b8f1e071b413f363fe4ea2e4d0903
ogr <ogr@ehtrace.com>
2023-09-01 16:05:30 +0200

    fix: correction 103

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 
-import {ref} from 'vue'
+import {computed} from 'vue'
 import ActionButton from "./components/ActionButton.vue"
-import {useRefHistory, useStorage, useFetch, useImage} from "@vueuse/core"
-import {computed, watch} from "@vue/runtime-core";
-import * as url from "url";
+import {useRefHistory, useStorage} from "@vueuse/core"
+import FreeCountInput from "./components/FreeCountInput.vue";
 
 const count = useStorage<number>('count', 200)
 
@@ -19,31 +18,12 @@ const dataButtons = [
   {id: 6, label: '+ 100', value: 100},
 ]
 
-const newCount = ref(count.value)
 
-const minValue = 100
-const maxValue = 599
-
-const baseUrl = ref('https://http.cat/images/')
-const inputHttpCode = ref(count.value)
-
-const urlSrcImg = computed(() => `${baseUrl}${inputHttpCode.value}.jpg`)
-
-const { data } = useFetch(urlSrcImg)
-
-
-function updateCount() {
-  const updatedCount = count.value + newCount.value
-  if (updatedCount >= minValue && updatedCount <= maxValue) {
-    count.value = updatedCount
-    commit()
-  }
-}
+const urlSrcImg = computed(() => `https://http.cat/images/${count.value}.jpg`)
 
 function clearCounter() {
   count.value = 200
   commit()
-  newCount.value = null
   clear()
 }
 
@@ -53,14 +33,13 @@ function clearCounter() {
 
   <main>
     <div>
-      <img alt="" :src="data">
+      <img alt="cat" :src="urlSrcImg" style="height: 400px">
     </div>
     <div>
       <h1 text-pink-600 uppercase text-4xl>Counter : <span text-black text-2xl>{{ count }}</span></h1>
     </div>
     <div>
-      <input type="number" v-model="newCount"/>
-      <button @click="updateCount" :disabled="!newCount">Update Count</button>
+      <FreeCountInput v-model="count"/>
     </div>
     <div>
       <ActionButton v-for="dataButton in dataButtons"
@@ -74,14 +53,11 @@ function clearCounter() {
       <button @click="clearCounter">Clear</button>
     </div>
 
-    <ul list-none>
-      <li v-for="i in history" :key="i">{{ i.snapshot }}</li>
+    <ul list-none style="position:absolute;">
+      <li v-for="(entry, i) in history" :key="i">{{ entry.snapshot }}</li>
     </ul>
 
   </main>
 
 </template>
 
-<style scoped>
-
-</style>
---
+++ src/components/FreeCountInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="number" v-model="newCount" @keydown.enter="updateCount"/>
+</template>
+<script lang="ts" setup>
+import {ref} from "vue";
+
+const minValue = 100
+const maxValue = 599
+
+const emits = defineEmits(['update:modelValue'])
+
+const newCount = ref<number | null>()
+
+function updateCount() {
+  if (!newCount.value)
+    return
+
+  if ((newCount.value >= minValue) && (newCount.value <= maxValue)) {
+    emits('update:modelValue', newCount.value)
+    newCount.value = null
+  }
+}
+
+</script>
